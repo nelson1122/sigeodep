8714dc0abd87c8e50dc2f936f41c401e31afbbf1
santosvaldesm@gmail.com <santosvaldesm@gmail.com@5f9e2f67-288d-9a8b-9ef6-54f1b2887f13>
2012-11-23 14:02:05 +0000

    Version para la realizacion de variables, incluye la BD 2

--- trunk/SIGEODEP/src/java/managedBeans/recordSets/RecordSetsLcenfMB.java
+++ trunk/SIGEODEP/src/java/managedBeans/recordSets/RecordSetsLcenfMB.java
@@ -202,7 +202,7 @@ public class RecordSetsLcenfMB implements Serializable {
             tuplesNumber = Integer.parseInt(totalRecords);
             tuplesProcessed = 0;
 
-            int rowPosition = 1;
+            int rowPosition = 0;
             HSSFWorkbook book = (HSSFWorkbook) document;
             HSSFSheet sheet = book.getSheetAt(0);// Se toma hoja del libro
             HSSFRow row;
--- trunk/SIGEODEP/src/java/managedBeans/recordSets/RecordSetsTransitMB.java
+++ trunk/SIGEODEP/src/java/managedBeans/recordSets/RecordSetsTransitMB.java
@@ -210,7 +210,7 @@ public class RecordSetsTransitMB implements Serializable {
             tuplesNumber = Integer.parseInt(totalRecords);
             tuplesProcessed = 0;
 
-            int rowPosition = 1;
+            int rowPosition = 0;
             HSSFWorkbook book = (HSSFWorkbook) document;
             HSSFSheet sheet = book.getSheetAt(0);// Se toma hoja del libro
             HSSFRow row;
