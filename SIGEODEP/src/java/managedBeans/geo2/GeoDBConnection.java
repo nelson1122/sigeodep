6dda9cdffb260b9ea16ca3d6499e23a062de57ed
andress.calderon <andress.calderon@gmail.com>
2013-05-24 23:59:39 +0000

    Adding functionalities to pie charts...

--- SIGEODEP/src/java/managedBeans/geo2/GeoDBConnection.java
+++ SIGEODEP/src/java/managedBeans/geo2/GeoDBConnection.java
@@ -105,7 +105,6 @@ public class GeoDBConnection implements Serializable {
     }
 
     public void refreshIndicatorData(int user_id, int indicator_id, ArrayList<Variable> order) {
-        System.out.println(user_id + " - " + indicator_id);
         this.setUser_id(user_id);
         this.setIndicator_id(indicator_id);
         this.order = order;
@@ -125,6 +124,13 @@ public class GeoDBConnection implements Serializable {
                 this.setGeo_column(column_order);
                 this.bins = 3;
                 this.getCommunesNumbers();
+                break;
+            }
+            if (column.getName().equalsIgnoreCase("cuadrante")) {
+                column_order += order2;
+                this.setGeo_column(column_order);
+                this.bins = 3;
+                this.getQuadrantsNumbers();
                 break;
             }
             order2++;
@@ -448,17 +454,128 @@ public class GeoDBConnection implements Serializable {
         }
     }
 
-    public String getPieData(String WHERE, int user_id, int indicator_id) {
+    public ArrayList<Double> getQuadrantsNumbers() {
+        numbers = new ArrayList<>();
         String query = "SELECT "
-                + "	min(record_id) AS id, column_2 AS label, sum(count) AS count "
+                + "        count "
+                + " FROM "
+                + "         quadrants "
+                + " INNER JOIN "
+                + "         (SELECT "
+                + "                 min(record_id) AS record_id, " + geo_column + ", sum(count) AS count "
+                + "         FROM "
+                + "                 indicators_records "
+                + "         WHERE "
+                + "                 count > 0 "
+                + "                 AND user_id = " + user_id + " "
+                + "                 AND indicator_id = " + indicator_id + " "
+                + "         GROUP BY "
+                + "                 " + geo_column + " "
+                + "         ORDER BY "
+                + "                 2) AS foo "
+                + " ON 	"
+                + "      (" + geo_column + " LIKE quadrant_name)  "
+                + " WHERE "
+                + "         geom IS NOT NULL "
+                + " ORDER BY "
+                + "         count";
+        System.out.println(query);
+        ResultSet records = this.consult(query);
+        try {
+            while (records.next()) {
+                numbers.add(new Double(records.getString("count")));
+            }
+            return numbers;
+        } catch (SQLException ex) {
+            Logger.getLogger(GeoDBConnection.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public List<MfFeature> getQuadrantsPolygons(RangeFactory rf) {
+        String query = "SELECT "
+                + "        record_id, quadrant_name, count, geom "
+                + " FROM "
+                + "         quadrants "
+                + " INNER JOIN "
+                + "         (SELECT "
+                + "                 min(record_id) AS record_id, " + geo_column + ", sum(count) AS count "
+                + "         FROM "
+                + "                 indicators_records "
+                + "         WHERE "
+                + "                 count > 0 "
+                + "                 AND user_id = " + user_id + " "
+                + "                 AND indicator_id = " + indicator_id + " "
+                + "         GROUP BY "
+                + "                 " + geo_column + " "
+                + "         ORDER BY "
+                + "                 2) AS foo "
+                + " ON 	"
+                + "      (" + geo_column + " LIKE quadrant_name)  "
+                + " WHERE "
+                + "         geom IS NOT NULL "
+                + " ORDER BY "
+                + "         count DESC";
+        WKTReader wktReader = new WKTReader();
+        List<MfFeature> polygons = new ArrayList<>();
+        ResultSet records = this.consult(query);
+        try {
+            while (records.next()) {
+                final int fid = records.getInt("record_id");
+                final String fname = records.getString("quadrant_name");
+                final Double fvalue = new Double(records.getInt("count"));
+                final String fcolour = "#" + rf.getColorByValue(fvalue);
+                String geom = records.getString("geom");
+                final MfGeometry fgeom = new MfGeometry(wktReader.read(geom));
+                MfFeature feature = new MfFeature() {
+                    String name = fname;
+                    private double value = fvalue;
+                    private String colour = fcolour;
+                    private Integer id = fid;
+
+                    @Override
+                    public String getFeatureId() {
+                        return id.toString();
+                    }
+
+                    @Override
+                    public MfGeometry getMfGeometry() {
+                        return fgeom;
+                    }
+
+                    @Override
+                    public void toJSON(JSONWriter writer) throws JSONException {
+                        writer.key("name");
+                        writer.value(name);
+                        writer.key("value");
+                        writer.value(value);
+                        writer.key("colour");
+                        writer.value(colour);
+                        writer.key("id");
+                        writer.value(id);
+                    }
+                };
+                polygons.add(feature);
+            }
+            return polygons;
+        } catch (ParseException | SQLException ex) {
+            Logger.getLogger(GeoDBConnection.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public String getPieData(String WHERE, String geo_column, String column, int user_id, int indicator_id) {
+        String query = "SELECT "
+                + "	min(record_id) AS id, " + column + " AS label, sum(count) AS count "
                 + "FROM "
                 + "	indicators_records "
                 + "WHERE "
-                + "	column_1 IN " + WHERE + " "
+                + "	" + geo_column +" IN " + WHERE + " "
                 + "     AND user_id = " + user_id + " "
                 + "     AND indicator_id = " + indicator_id + ""
                 + "GROUP BY "
-                + "	column_2 "
+                + "	" + column + " "
                 + "ORDER BY "
                 + "	1";
         ResultSet records = this.consult(query);
@@ -471,7 +588,7 @@ public class GeoDBConnection implements Serializable {
                 String label = records.getString("label");
                 int count = records.getInt("count");
                 if (count != 0) {
-                    labels.put(label);
+                    labels.put(label + " %% [###]");
                     values.put(count);
                 }
             }
